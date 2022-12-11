ad08c89eab62e5fddbeb85c388fda1274c17433d
lunacastrocamila <cluna20908@ufide.ac.cr>
2022-12-11 17:59:39 -0600

    Score

--- ProyectoGreenForest/Assets/Scripts/EnemyEventSystem/EnemyEventSystem.cs
+++ ProyectoGreenForest/Assets/Scripts/EnemyEventSystem/EnemyEventSystem.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyEventSystem : MonoBehaviour
 {
 
     [SerializeField]
     Transform damegeEffect;
+
+    [SerializeField]
+    TMP_Text score;
 
     void Start()
     {
@@ -18,7 +22,13 @@ public class EnemyEventSystem : MonoBehaviour
     void OnDie(GameObject go)
     {
         Destroy(go);
+
+        int value = int.Parse(score.text);
+        value = value + 5;
+        score.text = value.ToString();
         //StartCoroutine(OnDieCoroutine(go));
+
+
     }
 
     IEnumerator OnDieCoroutine(GameObject go)
